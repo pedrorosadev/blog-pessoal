e1a71ccf41ef261783f92051092ff94d8b766852
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-10-10 12:55:40 -0300

    Desenvolvimento do Blog Pessoal

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = 4000;
+
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(); //Permite que requisições de fora sejam aceitas
+
+  await app.listen(port);
+}
+bootstrap();
