ce4d3b96e653558565770648b6aa4513a07f2585
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-10-24 12:22:52 -0300

    Aplicando Segurança no Blog

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from "class-validator";
 import { Tema } from "src/tema/entities/tema.entity";
+import { Usuario } from "src/usuario/entities/usuario.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: 'tb_postagens'})
@@ -24,4 +25,8 @@ export class Postagem{
     })
     tema: Tema;
 
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: 'CASCADE'
+    })
+    usuario: Usuario;
 }
