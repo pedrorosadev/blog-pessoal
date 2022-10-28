5e807e21dc9008cd6950306a204af9a17ddc3baf
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-10-28 11:36:41 -0300

    Documentação com Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 4000;
+
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact('Generation Brasil', 'http://www.generationbrasil.online','generation@email.com')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,25 +1,29 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post, Put, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { Usuario } from "../entities/usuario.entity";
 import { UsuarioService } from "../services/usuario.service";
 
+@ApiTags('Usuario')
 @Controller('/usuarios')
 export class UsuarioController{
     constructor(private readonly usuarioService: UsuarioService){}
 
+    @ApiBearerAuth()
     @UseGuards(JwtAuthGuard)
     @Get('/all')
     @HttpCode(HttpStatus.OK)
     async findAll():Promise<Usuario[]>{
         return await this.usuarioService.findAll();
     }
-
+    
     @Post('/cadastrar')
     @HttpCode(HttpStatus.CREATED)
     async create(@Body() usuario: Usuario): Promise<Usuario>{
         return await this.usuarioService.create(usuario);
     }
 
+    @ApiBearerAuth()
     @UseGuards(JwtAuthGuard)
     @Put('/atualizar')
     @HttpCode(HttpStatus.OK)
